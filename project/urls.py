60d02f38cf3f467e052bf8daee7657b030f6bef5
Alyne Nayely Regalado Morales <alyn.remo@gmail.com>
2023-03-10 20:34:40 -0500

    Template intermedio funcional

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from SociaLTravel.views import index
+
+
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
+
+    
 ]
